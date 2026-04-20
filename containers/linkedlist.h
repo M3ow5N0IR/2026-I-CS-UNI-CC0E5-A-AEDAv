c75a75cf334c291ea3faffc7363e8d922662c62f
M3ow5N0IR <andrei.trujillo.a@uni.pe>
2026-04-19 22:55:33 -0500

    fix(T13): Concurrencia y bugs

--- containers/linkedlist.h
+++ containers/linkedlist.h
@@ -261,8 +261,8 @@ public:
     }
 };
 
-template <typename T>
-void LinkedList<T>::internal_insert(Node* &pPr  ev, const value_type &value, Ref ref){
+template <typename Trait>
+void LinkedList<Trait>::internal_insert(Node* &pPrev, const value_type &value, Ref ref){
     if(!pPrev || m_comp(value, pPrev->getDataRef())){
         pPrev = new Node(value, ref, pPrev);
         m_size++;
@@ -273,8 +273,10 @@ void LinkedList<T>::internal_insert(Node* &pPr  ev, const value_type &value, Ref
     internal_insert(pPrev->getNextRef(), value, ref);
 }
 
-template <typename T>
-void LinkedList<T>::insert(const value_type &value, Ref ref){
+template <typename Trait>
+void LinkedList<Trait>::insert(const value_type &value, Ref ref){
+    // T13
+    unique_lock<shared_mutex> lock(m_mtx);
     internal_insert(m_pRoot, value, ref);
 }
 
